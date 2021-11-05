fc5dc343611a2e3fc6f0fea667d944169a422b71
Andreea215 <oana0osanu@gmail.com>
2021-11-05 19:45:20 +0200

    feature: post and patch playlist collection

--- src/public/js/playlists-list.js
+++ src/public/js/playlists-list.js
@@ -5,3 +5,48 @@ const userName = await getUserDisplayName();
 userNameElement.innerHTML = userName;
 
 hasPremiumAccount();
+
+async function postPlaylistCollection(userId, playlistId, playlistType) {
+    try {
+        await fetch('http://127.0.0.1:8080/playlists',
+            {
+                method: "POST",
+                headers: {
+                    Accept: "*/*",
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                userId: userId,
+                playlists: [
+                        {
+                            id: playlistId,
+                            playlistType: playlistType,
+                        }
+                    ],
+                }),
+            }
+        )
+        console.log('Yaay!');
+    } catch {
+        console.log('Didn\'t work :(');
+    }
+}
+
+async function patchPlaylistCollection(userId, playlistId, playlistType) {
+    try {
+        await fetch(`http://127.0.0.1:8080/playlists/updateCollection/${userId}?playlistId=${playlistId}&playlistType=${playlistType}`,
+            {
+                method: "PATCH",
+                headers: {
+                    Accept: "*/*",
+                    "Content-Type": "application/json",
+                },
+            }
+        )
+        console.log('Yaay!');
+    } catch {
+        console.log('Didn\'t work :(');
+    }
+}
+
+//patchPlaylistCollection('aeg', 'jf564', 'around-world');

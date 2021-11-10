e892b2e9f419a782538ea5fe2a2c937ccb388be6
Andreea Lungu <andreealungu238@gmail.com>
2021-11-10 09:22:49 +0200

    feature: footer

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -4,9 +4,11 @@ const currentUser = await getUserDisplayName();
 const createOwnCardElement = document.getElementById("create-own");
 const musicFestivalCardElement = document.getElementById("music-festival");
 const aroundWorldCardElement = document.getElementById("around-world");
+const myPlaylistElement=document.getElementById("my-playlists");
 
 if(!currentUser) {
     createOwnCardElement.href = "./authorization.html";
     musicFestivalCardElement.href = "./authorization.html";
     aroundWorldCardElement.href = "./authorization.html";
+    myPlaylistElement.href = "./authorization.html";
 }
